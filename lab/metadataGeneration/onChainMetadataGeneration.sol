b6c9c04551ab0c2fa293c31a1f7c98d46b8c4b11
Remix Workspace <>
2022-11-07 13:56:46 -0500

    generate name of each coin dynamically on chain!

---
+++ lab/metadataGeneration/onChainMetadataGeneration.sol
@@ -0,0 +1,32 @@
+// SPDX-License-Identifier: GPL-3.0
+
+pragma solidity >=0.7.0 <0.9.0;
+
+import "@openzeppelin/contracts/utils/Strings.sol"; 
+import "@openzeppelin/contracts/utils/Base64.sol";
+
+contract OnChainMetadataGeneration{
+    function generateImage (uint nameNum) public pure returns (string memory) {
+        bytes memory json = abi.encodePacked(
+            '{',
+            '"name": "ColorCoin#',
+            Strings.toString(nameNum),
+            '",\n',
+            '"description": "Unique NFTs with color-changing system with every trade. ",\n', 
+            '"image": "___"\n '
+            '}'
+
+        );
+
+    return string(
+        json
+    )
+    ;
+        // return string (
+        //     abi.encodePacked(
+        //         'data:application/json;base64,', 
+        //         Base64.encode(json)
+        //     )
+        // );
+    }
+}
