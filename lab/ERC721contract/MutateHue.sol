ced7f60df5be5b2a8c353cac02414e3b454172db
lnrdmlt <leomalott@gmail.com>
2022-11-21 16:03:42 -0500

    make different random functions in code

---
+++ lab/ERC721contract/MutateHue.sol
@@ -0,0 +1,13 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.9;
+
+// contract mutateHue {
+ 
+//     function mutateHue(int hue) public view returns (int){
+//         // if (randomNum() % 2 == 0){
+//         //     return hue + 10;
+//         // }
+//         return hue - 10;
+//     }
+
+// }
